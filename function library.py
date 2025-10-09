6081b53db6445408efbc165a8c0b8aff6bcfb0a8
dylankim123 <dylankim@umd.edu>
2025-10-09 13:14:35 -0400

    Update

--- function library.py
+++ function library.py
@@ -1,9 +1,11 @@
 #Assign User ID
 
 #User input monthly income
-
+income = input("Please enter your income for this month: ")
+income = float(income)
 #User input monthly spending
-
+spending = input("Please enter your spending for this month: ")
+spending = float(spending)
 #Categorize spendings into different groups (Ex. Food, Rent, entertainment)
 
 #How will the data be stored/saved?
