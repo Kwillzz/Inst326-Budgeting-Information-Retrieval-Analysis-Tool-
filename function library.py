793c828d9633f01801d1c2e2295b330bb7a7c75a
dylankim123 <dylankim@umd.edu>
2025-10-09 13:20:59 -0400

    Update

--- function library.py
+++ function library.py
@@ -6,6 +6,11 @@ income = float(income)
 #User input monthly spending
 spending = input("Please enter your spending for this month: ")
 spending = float(spending)
+#How will the data be stored/saved?
+with open("budget.txt", "a") as file:
+  file.write(f"Income: {income}, Spending: {spending}\n")
+
+
 #Categorize spendings into different groups (Ex. Food, Rent, entertainment)
 
 #How will the data be stored/saved?
